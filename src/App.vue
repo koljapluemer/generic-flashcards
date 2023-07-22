366cdd53b01e05c3edfd68b3552d54367c87f749
blackbrokkoli <kastomat@gmail.com>
2023-07-22 16:43:26 +0200

    improve localData loader

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,14 @@
     />
   </div>
 
-  <hr>
+  <hr />
 
   <!-- list all flashcards with all info for debugging -->
   <ul>
     <li v-for="card in flashcards" :key="card.id">
-      {{ card.front }} - {{ card.back }} - {{ card.interval }} - {{ card.repetitions }} - {{ card.easinessFactor }} - {{ card.dueAt }}
+      {{ card.front }} - {{ card.back }} - {{ card.interval }} -
+      {{ card.repetitions }} - {{ card.easinessFactor }} - {{ card.dueAt }}
     </li>
-
   </ul>
 </template>
 
@@ -37,7 +37,19 @@ export default {
   created() {
     // Load flashcards from localStorage if available
     const savedFlashcards = JSON.parse(localStorage.getItem("flashcards"));
-    this.flashcards = savedFlashcards || this.initialFlashcards;
+
+    if (savedFlashcards) {
+      // Merge saved flashcards with the initialFlashcards list to preserve any new flashcards
+      this.flashcards = this.initialFlashcards.map((initialCard) => {
+        const savedCard = savedFlashcards.find(
+          (card) => card.id === initialCard.id
+        );
+        return savedCard ? { ...initialCard, ...savedCard } : initialCard;
+      });
+    } else {
+      // If no saved flashcards, use the initialFlashcards list as is
+      this.flashcards = this.initialFlashcards;
+    }
   },
   computed: {
     initialFlashcards() {
@@ -61,14 +73,131 @@ export default {
           easinessFactor: 2.5,
           dueAt: null,
         },
-        // Add more countries and capitals here with their respective interval, repetitions, and easinessFactor values
+        {
+          id: 3,
+          front: "Benin",
+          back: "Porto-Novo",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 4,
+          front: "Botswana",
+          back: "Gaborone",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 5,
+          front: "Burkina Faso",
+          back: "Ouagadougou",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 6,
+          front: "Burundi",
+          back: "Bujumbura",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 7,
+          front: "Cabo Verde",
+          back: "Praia",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 8,
+          front: "Cameroon",
+          back: "Yaoundé",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 9,
+          front: "Central African Republic",
+          back: "Bangui",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 10,
+          front: "Chad",
+          back: "N'Djamena",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 11,
+          front: "Comoros",
+          back: "Moroni",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 12,
+          front: "Congo",
+          back: "Brazzaville",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 13,
+          front: "Congo, Democratic Republic of the",
+          back: "Kinshasa",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 14,
+          front: "Djibouti",
+          back: "Djibouti",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
+        {
+          id: 15,
+          front: "Egypt",
+          back: "Cairo",
+          interval: 1,
+          repetitions: 0,
+          easinessFactor: 2.5,
+          dueAt: null,
+        },
       ];
     },
     dueFlashcards() {
       // Filter out flashcards that are due for review
       const currentDate = new Date();
       return this.flashcards.filter(
-        (card) => (card.dueAt && new Date(card.dueAt) <= currentDate) || !card.dueAt
+        (card) =>
+          (card.dueAt && new Date(card.dueAt) <= currentDate) || !card.dueAt
       );
     },
   },
