927aa33353687952af4d9f785e981bcd58e74965
blackbrokkoli <kastomat@gmail.com>
2023-07-23 17:51:44 +0200

    switch to comp api

--- src/App.vue
+++ src/App.vue
@@ -29,211 +29,108 @@
   </div>
 </template>
 
-<script>
-// import Flashcard from "./components/Flashcard.vue";
+<script setup>
+import { ref, reactive, computed, onMounted } from "vue";
 
-export default {
-  components: {},
-  data() {
-    return {
-      currentIndex: 0,
-      isRevealed: false,
-    };
+const currentIndex = ref(0);
+const isRevealed = ref(false);
+
+const initialFlashcards = [
+  {
+    id: 42,
+    front: "Senegal",
+    back: "Dakar",
+    dueAt: null,
+    repetitions: [],
+    interval: 1,
   },
-  created() {
-    // Load flashcards from localStorage if available
-    const savedFlashcards = JSON.parse(localStorage.getItem("flashcards"));
-    // const savedFlashcards = false;
+  {
+    id: 43,
+    front: "Seychelles",
+    back: "Victoria",
+    dueAt: null,
+    repetitions: [],
+    interval: 1,
+  },
+];
 
-    if (savedFlashcards) {
-      // Merge saved flashcards with the initialFlashcards list to preserve any new flashcards
-      this.flashcards = this.initialFlashcards.map((initialCard) => {
+// Load flashcards from localStorage if available
+const savedFlashcards = JSON.parse(localStorage.getItem("flashcards"));
+
+const flashcards = reactive(
+  savedFlashcards
+    ? initialFlashcards.map((initialCard) => {
         const savedCard = savedFlashcards.find(
           (card) => card.id === initialCard.id
         );
         return savedCard ? { ...initialCard, ...savedCard } : initialCard;
-      });
-    } else {
-      // If no saved flashcards, use the initialFlashcards list as is
-      this.flashcards = this.initialFlashcards;
-      console.log(this.flashcards);
-    }
+      })
+    : initialFlashcards
+);
 
-    this.nextCard();
-  },
-  computed: {
-    initialFlashcards() {
-      // Add your initial list of flashcards here
-      return [
-        {
-          id: 42,
-          front: "Senegal",
-          back: "Dakar",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 43,
-          front: "Seychelles",
-          back: "Victoria",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 44,
-          front: "Sierra Leone",
-          back: "Freetown",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 45,
-          front: "Somalia",
-          back: "Mogadishu",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 46,
-          front: "South Africa",
-          back: "Pretoria (administrative) / Cape Town (legislative) / Bloemfontein (judicial)",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 47,
-          front: "South Sudan",
-          back: "Juba",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 48,
-          front: "Sudan",
-          back: "Khartoum",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 49,
-          front: "Tanzania",
-          back: "Dodoma (political) / Dar es Salaam (commercial)",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 50,
-          front: "Togo",
-          back: "Lomé",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 51,
-          front: "Tunisia",
-          back: "Tunis",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 52,
-          front: "Uganda",
-          back: "Kampala",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 53,
-          front: "Zambia",
-          back: "Lusaka",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-        {
-          id: 54,
-          front: "Zimbabwe",
-          back: "Harare",
-          dueAt: null,
-          repetitions: [],
-          interval: 1,
-        },
-      ];
-    },
-    dueFlashcards() {
-      // Filter out flashcards that are due for review
-      const currentDate = new Date()
-      const dueCards =this.flashcards.filter(
-        (card) =>
-          (card.dueAt && new Date(card.dueAt) <= currentDate) || !card.dueAt
-      );
-      console.log(`found ${dueCards.length} due cards`);
-    },
-    dueFlashcardsWithoutNew() {
-      // filter flashcards that have no repetitions
-      return this.dueFlashcards.filter((card) => card.repetitions.length > 0);
-    },
-    card() {
-      // Return the current flashcard
-      return this.dueFlashcards[this.currentIndex];
-    },
-  },
-  methods: {
-    calculateRapidSR(grade) {
-      // save answer in repetitons, with timestamp including seconds
-      const answer = {
-        timestamp: new Date().toISOString(),
-        answer: grade,
-      };
+const dueFlashcards = computed(() => {
+  // Filter out flashcards that are due for review
+  const currentDate = new Date();
+  return flashcards.filter(
+    (card) => (card.dueAt && new Date(card.dueAt) <= currentDate) || !card.dueAt
+  );
+});
 
-      this.flashcards[this.currentIndex].repetitions.push(answer);
-      // if grade is 0, half interval (min: 1), else multiply with 2
-      const interval =
-        grade === 0
-          ? Math.max(this.flashcards[this.currentIndex].interval / 2, 1)
-          : this.flashcards[this.currentIndex].interval * 2;
-      // add seconds to dueAt
-      const dueAt = new Date();
-      dueAt.setSeconds(dueAt.getSeconds() + interval);
-      this.flashcards[this.currentIndex].interval = interval;
-      // TODO: vue "reactiveness" apparently doesn't fucking extend to array contents?!?!
-      this.flashcards[this.currentIndex].dueAt = dueAt.toISOString();
-      // save the flashcards to localStorage
-      localStorage.setItem("flashcards", JSON.stringify(this.flashcards));
-      this.nextCard();
-    },
-    download() {
-      // download flashcards as json
-      const dataStr =
-        "data:text/json;charset=utf-8," +
-        encodeURIComponent(JSON.stringify(this.flashcards));
-      const downloadAnchorNode = document.createElement("a");
-      downloadAnchorNode.setAttribute("href", dataStr);
-      downloadAnchorNode.setAttribute(
-        "download",
-        `flashcard_data_${Date.now().toISOString}.json`
-      );
-      document.body.appendChild(downloadAnchorNode); // required for firefox
-      downloadAnchorNode.click();
-      downloadAnchorNode.remove();
-    },
-    nextCard() {
-      // Pick a random index from the dueFlashcards array
-      // Set the current index to the new index
-      this.currentIndex = Math.floor(Math.random() * this.dueFlashcards.length);
-      this.isRevealed = false;
-    },
-  },
-};
+
+const card = computed(() => {
+  // Return the current flashcard
+  return dueFlashcards.value[currentIndex.value];
+});
+
+function calculateRapidSR(grade) {
+  // Save answer in repetitions, with a timestamp including seconds
+  const answer = {
+    timestamp: new Date().toISOString(),
+    answer: grade,
+  };
+
+  flashcards[currentIndex.value].repetitions.push(answer);
+  // If grade is 0, half interval (min: 1), else multiply with 2
+  const interval =
+    grade === 0
+      ? Math.max(flashcards[currentIndex.value].interval / 2, 1)
+      : flashcards[currentIndex.value].interval * 2;
+  // Add seconds to dueAt
+  const dueAt = new Date();
+  dueAt.setSeconds(dueAt.getSeconds() + interval);
+  flashcards[currentIndex.value].interval = interval;
+  // Update dueAt using a new reactive object to trigger reactivity
+  flashcards[currentIndex.value] = {
+    ...flashcards[currentIndex.value],
+    dueAt: dueAt.toISOString(),
+  };
+  // Save the flashcards to localStorage
+  localStorage.setItem("flashcards", JSON.stringify(flashcards));
+  nextCard();
+}
+
+function download() {
+  // Download flashcards as JSON
+  const dataStr =
+    "data:text/json;charset=utf-8," +
+    encodeURIComponent(JSON.stringify(flashcards));
+  const downloadAnchorNode = document.createElement("a");
+  downloadAnchorNode.setAttribute("href", dataStr);
+  downloadAnchorNode.setAttribute(
+    "download",
+    `flashcard_data_${Date.now().toISOString()}.json`
+  );
+  document.body.appendChild(downloadAnchorNode); // Required for Firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
+function nextCard() {
+  // Pick a random index from the dueFlashcards array
+  currentIndex.value = Math.floor(Math.random() * dueFlashcards.value.length);
+  isRevealed.value = false;
+}
+
+// Call the nextCard function when the component is mounted
+onMounted(nextCard);
 </script>
